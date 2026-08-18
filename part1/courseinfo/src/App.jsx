0f95652df13e33dbdeb6584a7a727d78a3d47f1c
Raj Kumar Tiruwa <rajkumartiruwa280@gmail.com>
2026-08-18 16:07:14 +0545

    complete exercise-1.9

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -35,9 +35,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0);
-  
+  const total = good + neutral + bad;
   // display no feedback given if the total feedback is 0, else display the Statistic of the feedbacks
-  
+  if (total ===0){
+    return (
+    <>
+    <div>
+     <h1>give feedback</h1>
+     
+      <button onClick = {()=>setGood(good+1)}>good</button>
+      <button onClick = {()=>setNeutral(neutral+1)}>neutral</button>
+      <button onClick = {()=>setBad(bad+1)}>bad</button>
+    </div>
+    <h1>Statistics</h1>
+   <p>No feedback given</p>
+    </>
+    
+  )
+  } else{
     return (
     <>
     <div>
@@ -51,6 +66,8 @@ const App = () => {
     </>
     
   )
+  }
+    
   
 
   
