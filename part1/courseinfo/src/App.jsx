a2b4a907f3065dd9c9a8828682775ec572b6adf2
Raj Kumar Tiruwa <rajkumartiruwa280@gmail.com>
2026-08-18 16:34:49 +0545

    complete exercise-1.10 and learn how to resuse the same components in different methods

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,7 +3,10 @@ import { useState } from 'react'
 
 const  StatisticLine = ({text, value}) =>{
   return(
-    <p>{text}:{value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -26,11 +29,15 @@ const Statistics = ({ good, neutral, bad, average, positive }) => {
   return (
     <div>
       <h2>Statistics</h2>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={`${positive}%`} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={`${positive}%`} />
+        </tbody>
+      </table>
     </div>
   )
 }
